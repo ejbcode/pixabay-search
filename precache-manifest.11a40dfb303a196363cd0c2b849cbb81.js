bfd9ff121749ded8bfa75f0ae3aca9823cdcc527
Ernesto Briceño <ejbcode@gmail.com>
2020-07-19 22:37:00 -0400

    Updates

---
+++ precache-manifest.11a40dfb303a196363cd0c2b849cbb81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25e416a5cd495be6e6e4ecc75728e8cd",
+    "url": "/pixabay-search/index.html"
+  },
+  {
+    "revision": "fa0025c0d8baa5f972a0",
+    "url": "/pixabay-search/static/js/2.25f00877.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pixabay-search/static/js/2.25f00877.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c869dc6379d2ce37fb",
+    "url": "/pixabay-search/static/js/main.d8e10817.chunk.js"
+  },
+  {
+    "revision": "11323cc95265486df318",
+    "url": "/pixabay-search/static/js/runtime-main.5d10d0fa.js"
+  }
+]);
